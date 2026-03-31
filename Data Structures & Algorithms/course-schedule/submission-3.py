8bcf74e5a69ad409f3f472fa6546a20014184f18
Miłosz Cieśla <120517139+mbo009@users.noreply.github.com>
2026-03-31 19:44:20 +0200

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,32 @@
+from collections import defaultdict, deque
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = {i : [] for i in range(numCourses)}
+
+        for course, prerequisite in prerequisites:
+            graph[course].append(prerequisite)
+        
+        visited = [False] * numCourses
+        safe = [False] * numCourses
+        def is_cycle(i):
+            if visited[i]:
+                return True
+            if safe[i]:
+                return False
+            
+            visited[i] = True
+
+            for neighbor in graph[i]:
+                if is_cycle(neighbor):
+                    return True
+            
+            visited[i] = False
+            return False
+        
+        for i in range(numCourses):
+            if not safe[i]:
+                if is_cycle(i):
+                    return False
+        
+        return True
