06e27f0981b6e24b4facdda3024cec02a33b7241
Miłosz Cieśla <120517139+mbo009@users.noreply.github.com>
2026-04-10 18:43:21 +0200

    Add: buy-and-sell-crypto-with-cooldown - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        self.max_profit = 0
+        visited = {}
+
+        def profit_rec(i, profit, bought):
+            if i >= len(prices):
+                self.max_profit = max(self.max_profit, profit)
+                return
+
+            key = (i, bought)
+
+            if key in visited:
+                if bought:
+                    if visited[key] >= profit:
+                        return
+                else:
+                    if visited[key] >= profit:
+                        return
+
+            visited[key] = profit
+
+            if not bought:
+                profit_rec(i + 1, profit - prices[i], True)
+            else:
+                profit_rec(i + 2, profit + prices[i], False)
+
+            profit_rec(i + 1, profit, bought)
+
+        profit_rec(0, 0, False)
+        return self.max_profit
