8a1a66714a43e647338510546e2916a2e77d42ac
Miłosz Cieśla <120517139+mbo009@users.noreply.github.com>
2026-07-15 17:47:40 +0200

    Add: two-integer-sum - submission-12

---
+++ Data Structures & Algorithms/two-integer-sum/submission-12.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = dict()
+
+        for i, num in enumerate(nums):
+            if target - num in seen:
+                return [seen[target - num], i]
+            elif num not in seen:
+                seen[num] = i
+        
+        return []
