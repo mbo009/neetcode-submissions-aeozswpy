d1a1120705615510921a647f1e7b62293286e876
Miłosz Cieśla <120517139+mbo009@users.noreply.github.com>
2026-07-30 20:12:04 +0200

    Add: string-encode-and-decode - submission-11

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-11.py
@@ -0,0 +1,16 @@
+class Solution:
+    def __init__(self):
+        self.separator = "ø"
+    
+    def encode(self, strs: List[str]) -> str:
+        encoded = []
+
+        for word in strs:
+            encoded.append(self.separator)
+            encoded.append(word)
+        
+        return "".join(encoded)
+    
+    def decode(self, s: str) -> List[str]:
+        return s.split(self.separator)[1:]
+        
